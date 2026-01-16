f9a1ba389e32b927ab4d18e3968d7fc66cb4b0a0
julirmatos <juli.rmatos@gmail.com>
2026-01-16 16:41:24 -0300

    Autenticação

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-sml6.onrender.com'
+    baseURL: 'https://blogpessoal-sml6.onrender.com',
 }); 
 
 export const cadastrarUsuario = async (url: string, dados : Object, setDados:Function) => {
