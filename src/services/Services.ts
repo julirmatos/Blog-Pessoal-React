8c29433896010a03f70976382a3e71ec35204636
julirmatos <juli.rmatos@gmail.com>
2026-01-16 15:32:18 -0300

    Configuração Service

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-9jo1.onrender.com'
-});
+    baseURL: 'https://blogpessoal-sml6.onrender.com'
+}); 
 
 export const cadastrarUsuario = async (url: string, dados : Object, setDados:Function) => {
     const resposta = await api.post(url, dados);
