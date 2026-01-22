fa24ca6a65edf0558a8f093df06f6947166719ba
julirmatos <juli.rmatos@gmail.com>
2026-01-22 15:12:14 -0300

    Deploy Vercel

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-sml6.onrender.com'
+  baseURL: 'baseURL: import.meta.env.VITE_API_URL'
 })
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
